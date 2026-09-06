2c568ec9358e79126dd6b9026d2ec6f3bdc10027
Ishaan Trivedi <137341691+IshaanTrived1@users.noreply.github.com>
2026-09-06 14:09:35 -0500

    Update ALU_Testbench.v
    added comments for readability

--- ALU_Testbench.v
+++ ALU_Testbench.v
@@ -1,11 +1,12 @@
 module ALU_Testbench;
+    //creates variables we'll use to test the ALU such as the inputs (A, B, opcode), the result, and values to store tests that pass and total tests
     reg[3:0]  A, B;
     reg[2:0] Operation;
     wire[7:0] result;
     integer pass_count = 0;
     integer test_count = 0;
     
-
+    //instantiates our ALU in our testbech
     ALU ALUtest (
         .A(A),
         .B(B),
@@ -13,28 +14,29 @@ module ALU_Testbench;
         .result(result)
     );
 
-    task run_test; 
+    //task that tests the alu
+    task run_test; //task is run like this (a, b, opcode, expected_result, name)
         input[3:0] Ain, Bin;
         input[2:0] Operationin;
         input[7:0] Expected_result;
         input[127:0] test_name;
-        begin 
-            A = Ain;
+        begin //our test values are mapped over to the ALU's inputs
+            A = Ain; 
             B = Bin;
             Operation = Operationin;
-            #10;
-            test_count = test_count + 1;
-            if(result == Expected_result) begin
+            #10; //wait 10 time units
+            test_count = test_count + 1; //increases test count by 1
+            if(result == Expected_result) begin //what to do if we get the correct result
                 pass_count = pass_count + 1;
                 $display("%s: PASS (result=%0d)", test_name, result);
-            end else begin
+            end else begin //what to do for incorrect result
                 $display("%s: FAIL (expected=%d, result=%0d)", test_name, Expected_result, result);
             end
         end
     endtask
 
     initial begin
-        $dumpfile("alu_wave.vcd");
+        $dumpfile("alu_wave.vcd"); //dumps everything into a wave so we can see it in gtkwave
         $dumpvars(0, ALU_Testbench);
 
         run_test(4'd1, 4'd1, 3'b001, 8'd2, "ADD");
