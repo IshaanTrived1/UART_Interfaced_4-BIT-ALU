3f96dccb3d3ebe9bd4997e154565acd8b5cf9e03
AstroRehab <officialastrorehab@gmail.com>
2026-09-16 11:05:19 -0500

    Finished initial block

--- testing/ALU_tb.sv
+++ testing/ALU_tb.sv
@@ -1,28 +1,38 @@
-module alu_tb;
+module alu_tb;//module declaration
   reg [3:0] A, B;
   reg[2:0] Operation;
   wire [7:0] result;
 
 
-  ALU dut (
+  ALU dut ( //initializing the dut
     .A(A),
     .B(B),
     .Operation(Operation),
     .result(result)
   );
 
-  initial begin (
+
+  task automatic check_alu(input reg[2:0]op, input reg[3:0]a, input reg[3:0]b, input reg[7:0] expected); //our test function
+    A = a;
+    B = b;
+    Operation = op;
     #10;
 
-    A = 1; B = 1; Operation = 1;
+    if(result == expected)
+      $display("Test passed");
+    else 
+      $display("Test failed. Expected %d, got %d for operation: %d", expected, result, Operation);
+  endtask
 
-    #10;
 
-    if (result != 2)
-      $display("Test failed, expected 2 and recieved %d", result);
-    else
-      $display("Test passed!");
-  )
+  initial begin 
+    check_alu(dut.ADD, 4, 3, 7);
+
+    check_alu(dut.SUB, 9, 4, 5);
+    check_alu(dut.MULT, 3, 3, 9);
+    check_alu(dut.SHIFT_LEFT, 1, 2, 4);
+  end
+      
 
   
   
